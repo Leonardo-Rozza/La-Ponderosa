7de8670caf90f9286305f0cee77d8b8f7f695708
Leonardo-Rozza <leonardo155964@gmail.com>
2024-07-26 18:48:43 -0300

    Se agrego el calendario

---
+++ src/components/CalendarioReact.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import esLocale from "@fullcalendar/core/locales/es";
+import "./Calendario.css";
+
+interface CalendarioProps {
+  ocupados: string[];
+}
+
+const CalendarioReact: React.FC<CalendarioProps> = ({ ocupados }) => {
+  const events = ocupados.map((fecha) => ({
+    title: "Ocupado",
+    date: fecha,
+  }));
+
+  return (
+    <div className="calendar-container">
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        events={events}
+        locale={esLocale}
+        buttonText={{
+          today: "Hoy",
+        }}
+      />
+    </div>
+  );
+};
+
+
+export default CalendarioReact;
