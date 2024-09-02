55f6332060741f44698481dc4468d4bca46cead8
Leonardo-Rozza <Leonardo155964@gmail.com>
2024-09-01 21:32:38 -0300

    Se creo funcionalidad de btn-wsp para que desaparezca en menu movil

--- src/components/carrousel-menu.ts
+++ src/components/carrousel-menu.ts
@@ -31,11 +31,9 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-
   setInterval(() => {
     changeSlide("next");
   }, 3000);
-
 
   changeSlide("next");
 });
@@ -44,6 +42,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const $menuBtn = document.querySelector<HTMLButtonElement>(".menu-btn");
 const $menu = document.querySelector<HTMLElement>(".menu");
+const $whatsappButton = document.querySelector<HTMLElement>(".whatsapp-button");
 
 const $firstChild = $menuBtn?.firstElementChild as HTMLElement | null;
 const $lastChild = $menuBtn?.lastElementChild as HTMLElement | null;
@@ -52,6 +51,9 @@ $menuBtn?.addEventListener("click", (e: MouseEvent) => {
   $firstChild?.classList.toggle("none");
   $lastChild?.classList.toggle("none");
   $menu?.classList.toggle("is-active");
+
+  // Ocultar o mostrar el botón de WhatsApp
+  $whatsappButton?.classList.toggle("hidden");
 });
 
 document.addEventListener("click", (e: MouseEvent) => {
